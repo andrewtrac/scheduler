687b12dcd080061c7014f7c6f0a3fe8b01fb320d
Andrew Trac <andrewtrac@Andrews-Air.Home>
2020-06-25 22:29:59 -0400

    Pushing broken code to test CI

--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import { render } from "@testing-library/react";
 import Appointment from "components/Application";
 
-xit("renders without crashing", () => {
+it("renders without crashing", () => {
   render(<Appointment />);
 });
 
